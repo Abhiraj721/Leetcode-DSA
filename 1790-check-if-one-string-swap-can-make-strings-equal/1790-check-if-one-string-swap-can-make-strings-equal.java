6e0e42a21e47f9e2301b62011894f87b99946870
Abhiraj Bhosle <92145377+Abhiraj721@users.noreply.github.com>
2022-08-12 14:08:21 +0530

    Time: 1 ms (89.38%), Space: 40.2 MB (98.54%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.java
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.java
@@ -1,30 +1,19 @@
 class Solution {
     public boolean areAlmostEqual(String s1, String s2) {
-        int a_pointer=0;
-            if(s1.equals(s2))return true;
-            int b_pointer=s2.length()-1;
-            while(a_pointer<b_pointer){
-                    if(helper(a_pointer,b_pointer,s2 ,s1)){
-                            return true;
-                    }
-                if(s2.charAt(a_pointer)==(s1.charAt(a_pointer)))a_pointer++;
-                else if(s2.charAt(b_pointer)==(s1.charAt(b_pointer)))b_pointer--;
-                    else{
-                            a_pointer++;
-                            b_pointer--;
-                    }
-            }
+        if(s1.equals(s2))return true;
+            int []arr=new int[100];
+            int index=0;
+         for(int i=0;i<s2.length();i++){
+                 if(s1.charAt(i)!=s2.charAt(i))arr[index++]=i;
+         }
+            if(arr[2]!=0)return false;
+           char[] newarr=s2.toCharArray();
+            char temp=newarr[arr[0]];
+            newarr[arr[0]]=newarr[arr[1]];
+            newarr[arr[1]]=temp;
+            String new_s2=new String(newarr);
+            if(s1.equals(new_s2))return true;
             return false;
+            
     }
-        public boolean helper(int i,int j,String s2,String s1){
-                char s2arr[]=s2.toCharArray();
-                System.out.print(Arrays.toString(s2arr));
-               char  temp=s2arr[i];
-                s2arr[i]=s2arr[j];
-                s2arr[j]=temp;
-                String check=new String(s2arr);
-                if(check.equals(s1))return true;
-                return false;
-                
-        }
 }
