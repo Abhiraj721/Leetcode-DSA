ef2a224ee39ffe2f7b2bcb8b0b3666c16f89d648
Abhiraj Bhosle <92145377+Abhiraj721@users.noreply.github.com>
2022-08-13 17:08:04 +0530

    Time: 95 ms (5.38%), Space: 129.5 MB (5.11%) - LeetHub

--- 50-powx-n/50-powx-n.java
+++ 50-powx-n/50-powx-n.java
@@ -1,19 +1,32 @@
 class Solution {
     public double myPow(double x, int n) {
-       long n_copy=n;
-        double ans=1.0;
-            if(n_copy<0)n_copy*=-1;
-            while(n_copy>0){
-                if(n_copy%2==0){
-                        x=x*x;
-                        n_copy=n_copy/2;
-                }else{
-                        ans=ans*x;
-                        n_copy-=1;
-                }
+            if(x<=0.00001 && x>-0.00001){
+                    System.out.print("nice");
+                    return (double)0;
+            }
+            if(x==(double)1)return (double)1;
+            if(n==0)return 1;
+            boolean isnegative=false;
+            if(n<0){
+                    isnegative=true;
+                    n*=-1;
                     
-                       }
-             if(n<0)ans=1.0/ans;
-            return ans;
+            }
+                     if(n<-100000 || n>1000000){
+                             if(x<0){
+                                     if(n<0)return (double)1;
+                                     else{
+                                           return (double)-1;}
+                                     }
+                                     
+                        else{
+                                return (double)0;
+                        }
+                     }
+            double partial_ans=myPow(x,n-1);
+            if(isnegative){
+                    return  1.0/(x*partial_ans);
+            }
+            return x*partial_ans;
     }
 }
