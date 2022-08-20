ba661f04cc6c34496c26c7da230ae64f7afc2007
Abhiraj Bhosle <92145377+Abhiraj721@users.noreply.github.com>
2022-08-20 13:45:37 +0530

    Time: 5 ms (84.58%), Space: 42.5 MB (94.14%) - LeetHub

---
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] sortByBits(int[] arr) {
+        HashMap<Integer,List<Integer>>map=new HashMap<>();
+            Arrays.sort(arr);
+        List<Integer>li=new ArrayList<>();
+            for(int i=0;i<arr.length;i++){
+                    int num=Integer.bitCount(arr[i]);
+                    if(!map.containsKey(num))map.put(num,new ArrayList<>());
+                    map.get(num).add(arr[i]);
+            }
+                int []ans=new int [arr.length];
+                int index=0;
+            for(List<Integer>temp:map.values()){
+                    for(int i:temp){
+                            ans[index++]=i;
+                    }
+            }
+        return ans;
+    }
+}
