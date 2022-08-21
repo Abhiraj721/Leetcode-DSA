fa7cb645408bfa3b1067b749f4b92f14c3ee86c0
Abhiraj Bhosle <92145377+Abhiraj721@users.noreply.github.com>
2022-08-21 14:12:42 +0530

    Time: 934 ms (5.37%), Space: 54.9 MB (9.62%) - LeetHub

---
+++ 47-permutations-ii/47-permutations-ii.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+         List<List<Integer>>res=new ArrayList<>();
+            helper(res,0,nums.length,nums);
+            return res;
+    }
+        public void helper(List<List<Integer>>res,int pos,int n,int []nums){
+                if(pos>=n){
+                        List<Integer>sublist=Convert_to_ArrayList(nums);
+                        if(!res.contains(sublist))res.add(sublist);
+                        return ;
+                }
+                for(int i=pos;i<nums.length;i++){
+                        swap(i,pos,nums);
+                       helper(res,pos+1,n,nums);
+                        swap(i,pos,nums);
+                }
+        }
+        public List<Integer> Convert_to_ArrayList(int []nums){
+                List<Integer>ans=new ArrayList<>();
+                for(int i:nums){
+                        ans.add(i);
+                }
+                return ans;
+        }
+        public void swap(int i,int j,int nums[]){
+                int temp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+                
+        }
+    }
