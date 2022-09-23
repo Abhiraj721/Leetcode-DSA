da07e55c46e166ec31beea40f8af94f9b947d140
Abhiraj Bhosle <92145377+Abhiraj721@users.noreply.github.com>
2022-09-23 20:38:45 +0530

    Time: 1 ms (89.62%), Space: 42.8 MB (82.76%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -1,16 +1,18 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-      List<List<Integer>>res=new ArrayList<>();
-            backtrack(res,0,new ArrayList<>(),nums);
+        List<List<Integer>>res=new ArrayList<>();
+            backtrack(res,new ArrayList<Integer>(),0,nums);
             return res;
     }
-        public void backtrack(List<List<Integer>>res,int index,List<Integer>subsets,int []nums){
-                res.add(new ArrayList<>(subsets));
-                for(int i=index;i<nums.length;i++){
-                       subsets.add(nums[i]); //adding the current element to our subesets
-                        backtrack(res,i+1,subsets,nums);
-                        subsets.remove(subsets.size()-1);//removing the same element which we have added
+        public void backtrack(List<List<Integer>>res,List<Integer>subset,int index,int []nums){
+                if(index==nums.length){
+                        res.add(new ArrayList<>(subset));
+                        return;
                 }
+                subset.add(nums[index]);
+                backtrack(res,subset,index+1,nums);
+                subset.remove(subset.size()-1);
+                backtrack(res,subset,index+1,nums);
                 return;
         }
 }
